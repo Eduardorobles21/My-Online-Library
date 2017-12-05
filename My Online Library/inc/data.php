445d56806b10233e2ff3f97a5b63948f797eb415
Eduardorobles21 <34288550+Eduardorobles21@users.noreply.github.com>
2017-12-05 18:24:03 -0500

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,123 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Hunger Games",
+	"img" => "img/media/The_Hunger_Games.png",
+    "genre" => "Action",
+    "format" => "Paperback",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Suzanne Collins"
+    ],
+    "publisher" => "Scholastic",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+	"title" => "Girl with a dragon Tattoo",
+	"img" => "img/media/GIRLWITH THEDRAGON.png",
+    "genre" => "Drama",
+    "format" => "Paperback",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Stieg Larsson"
+    ],
+    "publisher" => "Scholastic",
+    "isbn" => '978-0201633610'
+];
+$catalog[103] = [
+	"title" => "Doctor Sleep",
+	"img" => "img/media/DOCTORSLEEP.png",
+    "genre" => "Drama",
+    "format" => "Paperback",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Stephen King"
+    ],
+    "publisher" => "Scribner",
+    "isbn" => '978-0201633610'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Annabelle",
+    "img" => "img/media/annabelle-creation.png",
+    "genre" => "Horror",
+    "format" => "DVD",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "John R. Leonetti",
+    "writers" => [
+        "Gary Dauberman",
+    ],
+    "stars" => [
+        "Annabelle Wallis",
+        "Ward Horton",
+        "Alfre Woodard"
+    ]
+];
+$catalog[202] = [
+    "title" => "Guardians of the Galaxy",
+    "img" => "img/media/GURDIANOFTHEGALAXY.png",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "James Gunn",
+    "writers" => [
+        "James Gunn",
+        "Nicole Perlman"
+    ],
+    "stars" => [
+        "Chriss Pratt",
+        "Zoe Saldana",
+        "Dave Bautista"
+    ]
+];
+$catalog[203] = [
+    "title" => "IT",
+    "img" => "img/media/IT.png",
+    "genre" => "Horror",
+    "format" => "DVD",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => "Andy Muschietti",
+    "writers" => [
+        "Chase Palmer",
+        "Cary Fukunaga"
+    ],
+    "stars" => [
+        "Jaeden Lieberher",
+        "Bill Skarsgård"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Everyone",
+    "img" => "img/media/LOGIC.png",
+    "genre" => "Hip-Pop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Logic"
+];
+$catalog[302] = [
+    "title" => "American Teen",
+    "img" => "img/media/KHALID.png",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Khalid"
+];
+$catalog[303] = [
+    "title" => "PlusAndEqual",
+    "img" => "img/media/MARTINGARRIX.png",
+    "genre" => "Techno",
+    "format" => "CD",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Martin Garrix"
+];
